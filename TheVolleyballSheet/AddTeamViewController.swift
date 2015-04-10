580f83df270e92d53581f4f12ec7a847befe2a59
EuphTodd <toddmccready@gmail.com>
2015-04-09 22:53:11 -0400

    Can add and display teams, sorted by in season/out of season
    *Bug - notifications aren't working yet

--- TheVolleyballSheet/AddTeamViewController.swift
+++ TheVolleyballSheet/AddTeamViewController.swift
@@ -7,9 +7,22 @@
 //
 
 import UIKit
+import CoreData
+
+protocol AddTeamViewControllerDelegate {
+    func addTeam(message: String)
+    func addTeamCanceled(message: String)
+}
 
 class AddTeamViewController: UIViewController {
 
+    var delegate:AddTeamViewControllerDelegate?
+    
+    @IBOutlet weak var teamNameTextField: UITextField!
+    @IBOutlet weak var leagueNameTextField: UITextField!
+    @IBOutlet weak var seasonTextField: UITextField!
+    @IBOutlet weak var inSeasonSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +34,35 @@ class AddTeamViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonPressed(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        delegate?.addTeamCanceled("Team was not added")
     }
-    */
+    
+    @IBAction func saveNewTeamButtonPressed(sender: UIButton) {
+        let appDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        
+        let managedObjectContext = appDelegate.managedObjectContext
+        let entityDescription = NSEntityDescription.entityForName("Teams", inManagedObjectContext: managedObjectContext!)
+        let team = Teams(entity: entityDescription!, insertIntoManagedObjectContext: managedObjectContext!)
+
+        team.teamName = teamNameTextField.text
+        team.leagueName = leagueNameTextField.text
+        team.season = seasonTextField.text
+        team.isCurrentSeason = inSeasonSwitch.on
+        
+        appDelegate.saveContext()
+        
+        var request = NSFetchRequest(entityName: "Teams")
+        var error:NSError? = nil
+        
+        var results:NSArray = managedObjectContext!.executeFetchRequest(request, error: &error)!
+        for res in results {
+            println(res)
+        }
+
+        self.dismissViewControllerAnimated(true, completion: nil)
+        delegate?.addTeam("Team Added")
+    }
 
 }
--- TheVolleyballSheet/TeamViewController.swift
+++ TheVolleyballSheet/TeamViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
-class TeamViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class TeamViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate, AddTeamViewControllerDelegate {
 
     @IBOutlet weak var teamTableView: UITableView!
+
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext!
+    var fetchedResultsController:NSFetchedResultsController = NSFetchedResultsController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        fetchedResultsController = getFetchedResultsController()
+        fetchedResultsController.delegate = self
+        fetchedResultsController.performFetch(nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,20 +33,96 @@ class TeamViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
 
     @IBAction func addTeamBarButtonItemPressed(sender: UIBarButtonItem) {
+        self.performSegueWithIdentifier("showTeamAdd", sender: self)
     }
     
     //UITableViewDataSource
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fetchedResultsController.sections!.count
     }
 
+    // formatting headers
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 25
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if fetchedResultsController.sections?.count == 1 {
+            let fetchedObjects = fetchedResultsController.fetchedObjects!
+            let testTeam:Teams = fetchedObjects[0] as Teams
+            if testTeam.isCurrentSeason == true {
+                return "Current Season"
+            } else {
+                return "Past Seasons"
+            }
+        }
+        else {
+            if section == 0 {
+                return "Current Season"
+            } else {
+                return "Past Seasons"
+            }
+        }
+        
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return fetchedResultsController.sections![section].numberOfObjects
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let thisTeam = fetchedResultsController.objectAtIndexPath(indexPath) as Teams
+        
+        var cell: TeamTableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as TeamTableViewCell
+        
+        cell.teamNameLabel.text = thisTeam.teamName
+        cell.leagueLabel.text = thisTeam.leagueName
+        cell.seasonLabel.text = thisTeam.season
+        cell.winsLabel.text = "0"
+        cell.lossesLabel.text = "0"
+        
+        return cell
     }
     
     //UITableViewDelegate
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
     }
+
+    // NSFetchedResultsControllerDelegate
     
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        teamTableView.reloadData()
+    }
+    
+    //Helper
+    
+    func taskFetchRequest() -> NSFetchRequest {
+        let fetchRequest = NSFetchRequest(entityName: "Teams")
+        let sortDescriptor = NSSortDescriptor(key: "teamName", ascending: true)
+        let currentSeasonDescriptor = NSSortDescriptor(key: "isCurrentSeason", ascending: false)
+        fetchRequest.sortDescriptors = [currentSeasonDescriptor, sortDescriptor]
+
+        return fetchRequest
+    }
+    
+    func getFetchedResultsController() -> NSFetchedResultsController {
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: taskFetchRequest(), managedObjectContext: managedObjectContext, sectionNameKeyPath: "isCurrentSeason", cacheName: nil)
+        return fetchedResultsController
+    }
+    
+    //AddTeamViewControllerDelegate
+    func addTeam(message: String) {
+        showAlert(message: message)
+    }
+    func addTeamCanceled(message: String) {
+        showAlert(message: message)
+    }
+    
+    func showAlert(message: String = "Congratulations") {
+        var alert = UIAlertController(title: "Change Made", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
